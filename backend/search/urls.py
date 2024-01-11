64d3305829a4265f9099e3a9d0e647bbc43294bc
Marcelo Canaparro <Marcelo.Canaparro@ctw.bmwgroup.com>
2024-01-11 14:22:21 +0000

    Adds algolia client

--- backend/search/urls.py
+++ backend/search/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("", views.SearchListView.as_view(), name="search")]
+urlpatterns = [
+    path("", views.SearchListView.as_view(), name="search"),
+    path("algolia/", views.SearchThroughAlgoliaListView.as_view(), name="search"),
+]
--- backend/search/views.py
+++ backend/search/views.py
@@ -2,8 +2,20 @@ from django.shortcuts import render
 from rest_framework import generics
 from products.models import Product
 from products.serializers import ProductSerializer
+from . import client
+from rest_framework.response import Response
 
 # Create your views here.
+
+
+class SearchThroughAlgoliaListView(generics.GenericAPIView):
+    def get(self, request, *args, **kwargs):
+        query = request.GET.get("q")
+        tag = request.GET.get("tag") or None
+        if not query:
+            return Response("", status=400)
+        results = client.perform_search(query, tags=tag)
+        return Response(results)
 
 
 class SearchListView(generics.ListAPIView):
